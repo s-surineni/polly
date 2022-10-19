9dbd1f54820f85b4f49c161e99271dc88012e762
sampath surineni <sampath.surineni@gmail.com>
2022-10-19 14:06:29 +0530

    added more tests

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,7 @@
+import datetime
+
+from django.utils import timezone
+
 from django.db import models
 
 
@@ -7,6 +11,11 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
